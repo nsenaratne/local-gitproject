dfebc88eb7aa90f1e83d961d653d03c7aec6e11f
nsenarathne <nsenarathne@gmail.com>
2021-01-04 16:34:33 +0100

    Completed Team Model

--- local/settings.py
+++ local/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'local.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'local_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qWerty@123',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -124,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'local/static'),
 ]
+
+# Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
